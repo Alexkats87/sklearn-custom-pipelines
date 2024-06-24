a85e1a3f84aa97bf0abbc7f7c6628f167f8ae6d7
Alexkats87 <kacalap1@gmail.com>
2024-06-24 15:49:22 -0300

    Fix: LogReg model

--- transformers/models.py
+++ transformers/models.py
@@ -16,8 +16,7 @@ logger = logging.getLogger()
 class CustomLogisticRegressionClassifier(BaseEstimator, ClassifierMixin):
     
     """
-    Fit LogReg model (statsmodels), perform iterative feature selection
-    excluding features with high p-values after every model fit
+    Fit LogReg model (statsmodels)
     
     Input features detected by regex, for ex.:
      - fr"^({NUM}|{CAT})\w+"  -  to filter features that start "num__" and "cat__"
@@ -26,21 +25,24 @@ class CustomLogisticRegressionClassifier(BaseEstimator, ClassifierMixin):
     
     p_value_max = 0.05
 
-    def __init__(self, features_pattern=fr"\w+{BIN}{WOE}$", features_lst=None): 
+    def __init__(self, features_pattern = fr"\w+{BIN}{WOE}$", features_lst=None): 
         self.estimator = None  
         self.features_lst = features_lst
         self.features_pattern = re.compile(features_pattern)
 
     def fit(self, X, y):
         
+        model_stats = None
+        
         if self.features_lst:
-            
             logger.info(f"Modeling: LogisticRegression with fixed set of features.")
-            
+            logger.debug(f" {self.features_lst}.")
             self.estimator = sm.Logit(
                 y, 
                 sm.add_constant(X[self.features_lst])
             ).fit(disp=0)
+            
+            model_stats = self.estimator.summary2().tables[1]
 
         else:
             
@@ -52,15 +54,14 @@ class CustomLogisticRegressionClassifier(BaseEstimator, ClassifierMixin):
                 )
             )
 
-            iter = 1
             while True:
 
-                model = sm.Logit(
+                self.estimator = sm.Logit(
                     y,
                     sm.add_constant(X[self.features_lst])
                 ).fit(disp=0)    
                 
-                model_stats = model.summary2().tables[1]
+                model_stats = self.estimator.summary2().tables[1]
                 
                 worst = model_stats['P>|z|'].idxmax()
                 worst_p = model_stats.loc[worst]['P>|z|']
@@ -70,32 +71,47 @@ class CustomLogisticRegressionClassifier(BaseEstimator, ClassifierMixin):
                     model_stats = model_stats.drop(worst)
                     self.features_lst.remove(worst)
                 
-                    logger.debug(f"==========================")
-                    logger.debug(f"Features cnt: {len(self.features_lst)}")
-                    logger.debug(f"Iter. {iter}, dropped:      {worst}")
-                    logger.debug(f"P-value:      {worst_p:0.4}")
+                    logger.info(f"==========================")
+                    logger.info(f"Features cnt: {len(self.features_lst)}")
+                    logger.info(f"Dropped:      {worst}")
+                    logger.info(f"P-value:      {worst_p:0.4}")
                     
                 else:
                     break
-                
-                iter += 1
 
         logger.info(f"Modeling: LogisticRegression - features amount:{len(self.features_lst)}")
         logger.info(f"Modeling: LogisticRegression - fit done.")
         logger.debug(model_stats)
-        
-        self.estimator = model
+
         return self
 
     def predict(self, X):
          
         prediction = self.estimator.predict(
             sm.add_constant(X[self.features_lst], has_constant='add')
-        )
+        ).to_list()
+        
+        # For testing: returns full dataset with prediction column
+        # if self.return_full_dataset:
+        #     X['y_pred'] = prediction
+        #     return X
+        
+        # For test and batch prediction : returns full dataset with prediction column
+        if X.shape[0] > 1:
+            X['y_pred'] = prediction
+            return X
+        
+        # For prod and single prediction: composes output JSON
+        features_bins_list = [f[:-5] for f in self.features_lst]
+        
+        output = {}
+        output['WOE'] = X[self.features_lst].round(3).to_dict(orient='records')[0]
+        output['Bins'] = X[features_bins_list].to_dict(orient='records')[0]
+        output['Score'] = round(prediction[0], 5)
         
         logger.info(f"Modeling: LogisticRegression - prediction done.")
         
-        return prediction
+        return output
     
     
 class CustomCatBoostClassifier(BaseEstimator, ClassifierMixin):

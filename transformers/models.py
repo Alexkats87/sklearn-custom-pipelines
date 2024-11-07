8a5f612e7044c12fe687f7625a4e2b67770fd261
Alexkats87 <kacalap1@gmail.com>
2024-11-07 19:42:56 -0300

    Updated CustomCatBoostClassifier
    - Adopted to different dataset on `fit` and `predict` stages
    - Limited iterations for feature elimination loop

--- transformers/models.py
+++ transformers/models.py
@@ -96,90 +96,96 @@ class CustomCatBoostClassifier(BaseEstimator, ClassifierMixin):
     Fit CatBoostClassifier model, perform iterative feature selection
     excluding features with low feature importance after every model fit.
     
-    Uses evaluation set for early stopping.
-    
-    Input features detected by prefixes: 
-    - "num__" for numerical
-    - "cat__" for categorical
-    
     """
-    
-    importance_min = 0.01
-
     def __init__(self, **params):
-        
         self.params = params
         self.estimator = CatBoostClassifier(**self.params)
-            
         self.cat_features_set = None
         self.num_features_set = None
 
     def set_params(self, **params):
-        for param, value in params.items():
-            setattr(self.estimator, param, value)
+        self.params.update(params)
+        self.estimator = CatBoostClassifier(**self.params)
         return self
 
-    def fit(self, X, y, **params):
+    def fit(self, X, y, eval_set=None, early_stopping_rounds=None, **params):
+        self.cat_features_set = set(filter(lambda x: re.match(r".*__bin$", x), X.columns))
+        self.num_features_set = set(filter(lambda x: re.match(r"^(num__)", x), X.columns))
 
-        self.cat_features_set = set(filter(lambda x: re.match(fr"^({CAT})", x), X.columns))
-        self.num_features_set = set(filter(lambda x: re.match(fr"^({NUM})", x), X.columns))
-
+        logger.info(f"Modeling: Catboost - params: {self.estimator.get_params()}")
         logger.info(f"Modeling: Catboost - Initial features amount: {len(self.cat_features_set | self.num_features_set)}")
 
+        # Feature selection process
         iter = 1
         while True:
-            
+
             self.estimator.fit(
                 X=X[list(self.cat_features_set | self.num_features_set)],
                 y=y,
                 cat_features=list(self.cat_features_set),
                 eval_set=(
-                    params['eval_set'][0][list(self.cat_features_set | self.num_features_set)],
-                    params['eval_set'][1]
+                    X.sample(frac=0.05, random_state=42)[list(self.cat_features_set | self.num_features_set)],
+                    y.sample(frac=0.05, random_state=42)
                 ),
+                early_stopping_rounds=early_stopping_rounds,
                 verbose=0,
             )
             
             importance_dct = dict(zip(
-                self.estimator.feature_names_, 
+                self.estimator.feature_names_,
                 self.estimator.get_feature_importance()
             ))
-            
-            features_to_drop_set = set([key for key, value in importance_dct.items() if value <= self.importance_min])
-            
-            if not features_to_drop_set:           
-                logger.info(f"Modeling: Catboost - Selected features amount: {len(self.cat_features_set | self.num_features_set)}")               
+
+            features_to_drop_set = set([key for key, value in importance_dct.items() if value <= 0.03])
+
+            if not features_to_drop_set:
+                logger.info(f"Modeling: Catboost - Selected features amount: {len(self.cat_features_set | self.num_features_set)}")
                 break
             else:
-                self.cat_features_set = self.cat_features_set - set(features_to_drop_set)
-                self.num_features_set = self.num_features_set - set(features_to_drop_set)
-                logger.debug(f"==========================")
-                logger.debug(f"Iter. {iter}, dropped features amt: {len(features_to_drop_set)}")
+                self.cat_features_set -= features_to_drop_set
+                self.num_features_set -= features_to_drop_set
+                logger.info(f"Iter. {iter}, dropped features amt: {len(features_to_drop_set)}")
 
             iter += 1
+            if iter == 15:
+                logger.info(f"Modeling: Catboost - Selected features amount: {len(self.cat_features_set | self.num_features_set)}")
+                break
 
-        iterations_count = self.estimator.get_best_iteration() + 1
-        logger.debug(f"Iterations_count: {iterations_count}")
+        try:
+            iterations_count = self.estimator.get_best_iteration() + 1
+            logger.info(f"Iters count: {iterations_count}")
+        except Exception as e:
+            logger.warning(f"Can't calc iters count: {e}")
 
+        self.params['cat_features'] = list(self.cat_features_set)
+        self.params['iterations'] = iterations_count
+        logger.info(f"Modeling: Catboost on fit - params: {self.estimator.get_params()}")
         
-        self.estimator.fit(
-            X=X[list(self.cat_features_set | self.num_features_set)],
-            y=y,
-            cat_features=list(self.cat_features_set),
-            eval_set=(
-                params['eval_set'][0][list(self.cat_features_set | self.num_features_set)],
-                params['eval_set'][1]
-            ),
-            verbose=0,
+        self.model_calibr = CalibratedClassifierCV(
+            base_estimator=CatBoostClassifier(**self.params),
+            method='sigmoid'
         )
 
+        self.model_calibr.fit(
+            X=X[list(self.cat_features_set | self.num_features_set)], 
+            y=y
+        )
+        
         logger.info(f"Modeling: Catboost - fit done.")
 
         return self
 
     def predict(self, X):
+
+        # Make sure that:
+        # - cat features are only `str` or `int` types
+        # - num features are only `float` type
+        X[list(self.cat_features_set)] = X[list(self.cat_features_set)].astype(str)
+        X[list(self.num_features_set)] = X[list(self.num_features_set)].astype(float)
         
-        prediction = self.estimator.predict_proba(X[list(self.cat_features_set | self.num_features_set)])
+        # During prediction pass features in exactly the same order as they were used to train Catboost
+        prediction = self.model_calibr.predict_proba(X[self.estimator.feature_names_])
         logger.info(f"Modeling: Catboost - prediction done.")
-        
+        logger.info("=" * 50)
+
         return prediction

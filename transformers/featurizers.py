6f21c254a26ad6cb5536accacdedd867773bc2fc
Alexkats87 <kacalap1@gmail.com>
2024-11-10 22:39:49 -0300

    Update featurizers.py - FeatureEliminationTransformer - added IV-based selection

--- transformers/featurizers.py
+++ transformers/featurizers.py
@@ -57,59 +57,137 @@ class FeatureEliminationTransformer(BaseEstimator, TransformerMixin):
     Dropping:
         - Duplicated features
         - Constant and quasi constant
-        
-    Input features detected by regex, for ex.:
-     - fr"^({NUM}|{CAT})\w+"  -  to filter features that start "num__" and "cat__"
-     - fr"\w+__bin__woe$"  -   to filter features that end with "__bin"
+        - From highly-correlated features groups selected the most powerful
+        - IV-based (information value) feature selection
+
     """
     
-    def __init__(self, features_pattern=fr"^({NUM}|{CAT})\w+", constant_share_thr=0.98):
-
+    def __init__(self, correlation_thr=0.8, constant_share_thr=0.98, corr_features_selector_bins=5, iv_min=0.02, corr_features_selector_strategy='equal_width'):
+        self.correlation_thr = correlation_thr
         self.constant_share_thr = constant_share_thr
-        self.features_pattern = re.compile(features_pattern)
+        self.iv_min = iv_min
+        self.corr_features_selector_bins = corr_features_selector_bins
+        self.corr_features_selector_strategy = corr_features_selector_strategy
         self.features_to_drop = set()
 
-    def fit(self, X, y):
+    @staticmethod
+    def _calculate_iv(X, y, columns):
+        """
+        Calculate the Information Value (IV) for multiple features.
         
-        self.features_set = set(
-            filter(
-                lambda x: re.match(
-                    self.features_pattern, x
-                    ), X.columns
-            
-                )
-            )
-            
-        logger.info(f"Feature elimination - initial features count:      {len(self.features_set)}")
+        Parameters:
+        X (pd.DataFrame): The input dataframe with features.
+        y (pd.Series): The target variable series.
+        columns (list): A list of columns for which to calculate IV.
+        
+        Returns:
+        dict: A dictionary with columns as keys and their IV as values.
+        """
+        iv_dict = {}
+        for feature in columns:
+            # Combine X[feature] and y into a DataFrame for processing
+            df = pd.concat([X[feature], y], axis=1)
+            df.columns = [feature, 'target']
+    
+            # Calculate the distribution of bad and good (target 1 and 0)
+            total_bad = df['target'].sum()
+            total_good = df['target'].count() - total_bad
+    
+            # Group by feature and calculate the WoE and IV
+            grouped = df.groupby(feature).agg({'target': ['sum', 'count']})
+            grouped.columns = ['bad', 'total']
+            grouped['good'] = grouped['total'] - grouped['bad']
+    
+            # To avoid division by zero, add a small constant to good and bad
+            grouped['bad_dist'] = grouped['bad'] / total_bad
+            grouped['good_dist'] = grouped['good'] / total_good
+            grouped['woe'] = np.log((grouped['good_dist'] + 1e-9) / (grouped['bad_dist'] + 1e-9))
+            grouped['iv'] = (grouped['good_dist'] - grouped['bad_dist']) * grouped['woe']
+    
+            # Sum IV for the feature
+            iv_dict[feature] = grouped['iv'].sum()
+        
+        return iv_dict
+
+    def fit(self, X, y):
+        X = X.copy()
+        self.features_to_drop = set()
+
+        self.cat_features_set = set(filter(lambda x: re.match(r".*__bin$", x), X.columns))
+        self.num_features_set = set(filter(lambda x: re.match(r"^(num__)", x), X.columns))
+        self.all_features_set = self.cat_features_set.union(self.num_features_set)
+        logger.info(f"Feature elimination - initial features count:      {len(self.all_features_set)}")
 
         # 1. Drop duplicated features
-        self.selector_dup = DropDuplicateFeatures(variables=list(self.features_set))
+        self.selector_dup = DropDuplicateFeatures(variables=list(self.all_features_set))
         self.selector_dup.fit(X)
-        self.features_set = self.features_set - set(self.selector_dup.features_to_drop_)
-        logger.info(f"Feature elimination - after dups dropping:         {len(self.features_set)}")
+        self.cat_features_set = self.cat_features_set - set(self.selector_dup.features_to_drop_)
+        self.num_features_set = self.num_features_set - set(self.selector_dup.features_to_drop_)
+        self.all_features_set = self.all_features_set - set(self.selector_dup.features_to_drop_)
+        self.features_to_drop = self.features_to_drop | set(self.selector_dup.features_to_drop_)
+        logger.info(f"Feature elimination - after dups dropping:         {len(self.all_features_set)}")
         
         # 2. Drop quazi-constant features       
-        self.selector_const = DropConstantFeatures(variables=list(self.features_set), tol=self.constant_share_thr)
+        self.selector_const = DropConstantFeatures(variables=list(self.all_features_set), tol=self.constant_share_thr)
         self.selector_const.fit(X)
-        self.features_set = self.features_set - set(self.selector_const.features_to_drop_)
+        self.cat_features_set = self.cat_features_set - set(self.selector_const.features_to_drop_)
+        self.num_features_set = self.num_features_set - set(self.selector_const.features_to_drop_)
+        self.all_features_set = self.all_features_set - set(self.selector_const.features_to_drop_)
+        self.features_to_drop = self.features_to_drop | set(self.selector_const.features_to_drop_)
+        logger.info(f"Feature elimination - after constants dropping:     {len(self.all_features_set)}")
+
+        # 3. Filter by IV (information value)
+        iv_dict = self._calculate_iv(X, y, list(self.cat_features_set))
+        iv_features_to_drop = [k for k,v in iv_dict.items() if ((v < self.iv_min) or (v > 0.45))]
+        self.cat_features_set = self.cat_features_set - set(iv_features_to_drop)
+        self.all_features_set = self.all_features_set - set(iv_features_to_drop)
+        self.features_to_drop = self.features_to_drop | set(iv_features_to_drop)
+        logger.info(f"Feature elimination - after IV filter:     {len(self.all_features_set)}")
         
-        logger.info(f"Feature elimination - after constants dropping:     {len(self.features_set)}")
+        # 4. Detect groups of corr features
+        self.selector_corr = DropCorrelatedFeatures(variables=list(self.num_features_set), threshold=self.correlation_thr)   
+        self.selector_corr.fit(X)
+        correlated_feature_sets = self.selector_corr.correlated_feature_sets_
+        logger.info(f"Feature elimination - groups of corr features:      {len(correlated_feature_sets)}")
+
+        # 5. Select best features from groups of corr. features using SelectByTargetMeanPerformance class
+        for feature_set in correlated_feature_sets:
+            
+            feature_lst = list(feature_set)
+
+            logger.debug(f"Group size:  {len(feature_lst)}")
+            logger.debug(f"Features:    {feature_lst}")
+            
+            mean_target_selector = SelectByTargetMeanPerformance(
+                bins=self.corr_features_selector_bins, 
+                strategy=self.corr_features_selector_strategy
+            )
+            mean_target_selector.fit(X[feature_lst], y)
+            
+            feature_best = max(
+                mean_target_selector.feature_performance_, 
+                key=lambda k: mean_target_selector.feature_performance_[k]
+            )
+            
+            feature_lst.remove(feature_best)          
+
+            logger.debug(f"Best:        {feature_best}")
+            
+            self.num_features_set = self.num_features_set - set(feature_lst)
+            self.all_features_set = self.all_features_set - set(feature_lst)
+            self.features_to_drop = self.features_to_drop | set(feature_lst)
+
+        logger.info(f"Feature elimination - initial features count:      {len(self.all_features_set)}.")
         logger.info(f"Feature elimination - fit done.")
-        
         return self
 
     def transform(self, X, y=None):
+        X = X.copy()
 
-        features_set = set(
-            filter(
-                lambda x: re.match(
-                    self.features_pattern, x
-                    ), X.columns
-                )
-            )
+        X = X.drop(list(self.features_to_drop), axis=1, errors='ignore')
 
-        X = X.drop(list(features_set - self.features_set), axis=1)
-
+        logger.info(f"Feature elimination - dropped  features count:   {len(self.features_to_drop)}.")
+        logger.info(f"Feature elimination - selected features count:   {len(self.all_features_set)}.")
         logger.info(f"Feature elimination - transform done.")
         
         if y is not None:
